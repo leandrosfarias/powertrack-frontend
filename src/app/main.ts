8c256885e06728f3ab07693b262dabb5632422d6
Leandro Farias <76781628+leandrosfarias@users.noreply.github.com>
2025-12-24 18:26:51 -0300

    Adicionando uso do Primevue na aplicação

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,10 +3,19 @@ import './style.css'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
 
 const app = createApp(App)
 
 app.use(router)
 app.use(createPinia())
+app.use(PrimeVue,
+    {
+        theme: {
+            preset: Aura
+        }
+    }
+)
 
 app.mount('#app')
