d67cb12f0ce886a00ac5728a2aafa7cea35d823a
Leandro Farias <76781628+leandrosfarias@users.noreply.github.com>
2025-12-24 18:27:27 -0300

    feat: Novo componente para Loading

---
+++ src/components/Loading.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import ProgressSpinner from 'primevue/progressspinner'
+
+const isLoading = ref(true)
+
+const props = defineProps<{
+    loading?: boolean
+}>()
+</script>
+
+<template>
+    <div v-if="isLoading" class="loader">
+        <ProgressSpinner />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.loader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+}
+</style>
