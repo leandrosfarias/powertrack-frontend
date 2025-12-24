97e5305adddc3760fed955f174c87648f39bf5d3
Leandro Farias <76781628+leandrosfarias@users.noreply.github.com>
2025-12-24 16:52:43 -0300

    feat: Componente de conteúdo principal

---
+++ src/modules/coach/components/MainContent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <main class="main-content">
+        <router-view />
+    </main>
+</template>
+
+<style scoped lang="scss">
+h2 {
+    color: #fff;
+}
+</style>
