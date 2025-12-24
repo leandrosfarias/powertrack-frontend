9f8bb09796bce826ba3388615ecbe6d5bc089542
Leandro Farias <76781628+leandrosfarias@users.noreply.github.com>
2025-12-24 18:29:37 -0300

    feat: Interface para definir Tipo Atleta

---
+++ src/modules/coach/types/types.ts
@@ -0,0 +1,10 @@
+export interface Athlete {
+    id: number;
+    complete_name: string;
+    email: string;
+    birthdate: string;
+    // created_at: string;
+    // active: boolean;
+    // height: number;
+    // weight: number;
+}
