41b6d522eb20e668bac411326b26a06d8ab802fe
Leandro Farias <76781628+leandrosfarias@users.noreply.github.com>
2025-12-24 18:29:14 -0300

    feat: Componente para listagem de Atletas

---
+++ src/modules/coach/pages/Athletes/ListAthletes.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { type Athlete } from '../../types/types';
+import AtheleteCard from './AtheleteCard.vue';
+
+const props = defineProps<{
+    athletes?: Array<Athlete>;
+}>()
+</script>
+
+<template>
+    <div class="list-athletes">
+        <AtheleteCard
+            v-for="athlete in athletes"
+            :key="athlete.id"
+            :athlete="athlete"
+        />
+    </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
