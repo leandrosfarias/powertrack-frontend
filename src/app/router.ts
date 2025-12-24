bdc8ac045179810457712c7971cc1e2a441305cc
Leandro Farias <76781628+leandrosfarias@users.noreply.github.com>
2025-12-24 16:50:15 -0300

    feat: Adicionando novas rotas para dashboard de Coac e rotas filhas associadas

--- src/app/router.ts
+++ src/app/router.ts
@@ -2,6 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AuthLayout from '../modules/auth/pages/AuthLayout.vue'
 import Login from '../modules/auth/pages/Login.vue'
 import Register from '../modules/auth/pages/Register.vue'
+import CoachDashboard from '../modules/coach/pages/CoachDashboard.vue'
+import { useAuthStore } from '../modules/auth/store/authStore'
+import Dashboard from '../modules/coach/pages/Dashboard.vue'
+import Athletes from '../modules/coach/pages/Athletes.vue'
 
 const routes = [
     {
@@ -18,13 +22,45 @@ const routes = [
                 name: 'Register',
                 component: Register
             }
-        ]
+        ],
+        meta: { requiresAuth: false },
+        name: 'login'
     },
+    {
+        path: '/coach/dashboard',
+        name: 'CoachDashboard',
+        component: CoachDashboard,
+        meta: { requiresAuth: true },
+        children: [
+            {
+                path: '',
+                name: 'dashboard',
+                component: Dashboard
+            },
+            {
+                path: 'athletes',
+                name: 'athletes',
+                component: Athletes
+            }
+        ]
+    }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore()
+
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        next({ path: '/' })
+    } else if (to.name === 'login' && authStore.isAuthenticated) {
+        next({ name: 'coach-dashboard' })
+    } else {
+        next()
+    }
 })
 
 export default router;
